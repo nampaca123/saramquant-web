41e21b0342679be617fefb5ce7c559a620507a8a
nampaca123 <nampaca123@gmail.com>
2026-02-24 13:27:46 +0900

    260224 landingPage1.41

--- src/constants/i18n.constants.ts
+++ src/constants/i18n.constants.ts
@@ -1,4 +1,4 @@
 import type { Language } from '@/types';
 
-export const defaultLanguage: Language = 'ko';
+export const defaultLanguage: Language = 'en';
 export const LANGUAGES: Language[] = ['ko', 'en'];
